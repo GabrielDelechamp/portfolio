26171a037c903a2509eaee606947542d430311d0
Gabriel Delechamp <gabriel.delechamp.pro@gmail.com>
2025-02-24 09:04:11 +0100

    adding projects

--- nextjs-dashboard/app/components/projects/projects.tsx
+++ nextjs-dashboard/app/components/projects/projects.tsx
@@ -14,52 +14,55 @@ interface Project {
 const projectsData: Project[] = [
   {
     id: 1,
-    title: "UI Animations",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
-    technologies: ["React"],
+    title: "Eazy Media Website",
+    description: `
+    Ce site web m'a été demandé dans le cadre d'un stage pour Eazy Media. À l'aide d'une 
+    maquette faite sur Figma, j'ai réalisé ce site.Le développement de ce site m'as permis 
+    d'approfondir et de repousser mes connaissances en HTML/CSS natif. 
+    `,
+    image: "/images/eazy-website.png",
+    technologies: ["HTML", "CSS"],
     category: "animation"
   },
   {
     id: 2,
-    title: "Tetris Game",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
+    title: "Leave management",
+    description: `
+    Ce projet fût un projet fil rouge pour l'apprentissage du framework Laravel. Il intéragit
+    avec une base de données et permet la gestion de CRUD, de droits et gère une restriction
+    d'accès en fonction des droits de l'utilisateur. Il gère ainsi de plus un système de
+    création de compte et de connexion.
+    `,
     image: "/images/uianimation.jpg",
-    technologies: ["React", "Vue"],
+    technologies: ["Laravel"],
     category: "game"
   },
   {
     id: 3,
-    title: "Ethereum App",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
-    technologies: ["Vue"],
+    title: "Carspot Analytics",
+    description: `
+    Ce projet est celui de mon deuxième stage. Il a pour but de traiter et d'analyser de 
+    façon graphique les données d'une application mobile associée à l'entreprise. Ainsi, 
+    sur Carspot Analytics, on y retrouve tout un tas de graphiques et de données sur les 
+    utilisateurs et les autres fonctionnalités de l'application mobile. On y retrouve de plus 
+    une gestion de CRUD.
+    `,
+    image: "/images/carspot_1.png",
+    technologies: ["React"],
     category: "blockchain"
   },
   {
     id: 4,
-    title: "UI Animations",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
+    title: "This Portfolio",
+    description: `
+    Designé par mes soins, ce portfolio est à mon image : sobre et innovant. Ce projet 
+    m'est utile dans mon parcours scolaire mais aussi et surtout pour montrer mon travail 
+    et mes compétences !
+    `,
+    image: "/images/portfolio.png",
     technologies: ["React"],
     category: "animation"
   },
-  {
-    id: 5,
-    title: "Tetris Game",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
-    technologies: ["React", "Vue"],
-    category: "game"
-  },
-  {
-    id: 6,
-    title: "Ethereum App",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
-    technologies: ["Vue"],
-    category: "blockchain"
-  }
 ];
 
 interface ProjectsProps {
@@ -85,7 +88,7 @@ const Projects: React.FC<ProjectsProps> = ({ filters }) => {
           <dd>&#125;)</dd>
         </dl>
       </p>
-      <div className="flex flex-wrap gap-6 py-[50px]">
+      <div className="flex flex-wrap gap-6 justify-center py-[50px]">
         {filteredProjects.length > 0 ? (
           filteredProjects.map((project) => (
             <div key={project.id} className="max-w-[370px] flex flex-col">
@@ -114,7 +117,7 @@ const Projects: React.FC<ProjectsProps> = ({ filters }) => {
                   </div>
                 </div>
                 <div className="p-4">
-                  <p className="text-gray-400 mb-4">{project.description}</p>
+                  <p className="text-gray-400 mb-4">{project.description.substring(0,50-3)+"..."}</p>
                   <Link href={`/projects/${project.id}`}>
                     <button className="bg-gray-800 text-gray-300 px-4 py-2 rounded hover:bg-gray-700 transition-colors">
                       View Project
--- nextjs-dashboard/app/projects/[id]/page.tsx
+++ nextjs-dashboard/app/projects/[id]/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 import React from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
-// Importez les données et définissez les types
 interface Project {
   id: number;
   title: string;
@@ -15,71 +14,82 @@ interface Project {
 const projectsData: Project[] = [
   {
     id: 1,
-    title: "UI Animations",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
-    technologies: ["React"],
+    title: "Eazy Media Website",
+    description: `
+    Ce site web m'a été demandé dans le cadre d'un stage pour Eazy Media. À l'aide d'une 
+    maquette faite sur Figma, j'ai réalisé ce site.Le développement de ce site m'as permis 
+    d'approfondir et de repousser mes connaissances en HTML/CSS natif. 
+    `,
+    image: "/images/eazy-website.png",
+    technologies: ["HTML", "CSS"],
     category: "animation"
   },
   {
     id: 2,
-    title: "Tetris Game",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
+    title: "Leave management",
+    description: `
+    Ce projet fût un projet fil rouge pour l'apprentissage du framework Laravel. Il intéragit
+    avec une base de données et permet la gestion de CRUD, de droits et gère une restriction
+    d'accès en fonction des droits de l'utilisateur. Il gère ainsi de plus un système de
+    création de compte et de connexion.
+    `,
     image: "/images/uianimation.jpg",
-    technologies: ["React", "Vue"],
+    technologies: ["Laravel"],
     category: "game"
   },
   {
     id: 3,
-    title: "Ethereum App",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
-    technologies: ["Vue"],
+    title: "Carspot Analytics",
+    description: `
+    Ce projet est celui de mon deuxième stage. Il a pour but de traiter et d'analyser de 
+    façon graphique les données d'une application mobile associée à l'entreprise. Ainsi, 
+    sur Carspot Analytics, on y retrouve tout un tas de graphiques et de données sur les 
+    utilisateurs et les autres fonctionnalités de l'application mobile. On y retrouve de plus 
+    une gestion de CRUD.
+    `,
+    image: "/images/carspot_1.png",
+    technologies: ["React"],
     category: "blockchain"
   },
   {
     id: 4,
-    title: "UI Animations",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
+    title: "This Portfolio",
+    description: `
+    Designé par mes soins, ce portfolio est à mon image : sobre et innovant. Ce projet 
+    m'est utile dans mon parcours scolaire mais aussi et surtout pour montrer mon travail 
+    et mes compétences !
+    `,
+    image: "/images/portfolio.png",
     technologies: ["React"],
     category: "animation"
   },
-  {
-    id: 5,
-    title: "Tetris Game",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
-    technologies: ["React", "Vue"],
-    category: "game"
-  },
-  {
-    id: 6,
-    title: "Ethereum App",
-    description: "Duis aute irure dolor in velit esse cillum dolore.",
-    image: "/images/uianimation.jpg",
-    technologies: ["Vue"],
-    category: "blockchain"
-  }
 ];
 
 const ProjectDetail: React.FC = () => {
-  const { id } = useParams(); // Récupère l'ID dans l'URL
-  const projectId = Number(id); // Convertit en nombre
-  const project = projectsData.find((project) => project.id === projectId); // Recherche du projet
+  const { id } = useParams();
+  const router = useRouter(); // Hook pour la navigation
+  const projectId = Number(id);
+  const project = projectsData.find((project) => project.id === projectId);
 
   if (!project) {
     return <p className="text-red-500">Project not found</p>;
   }
 
   return (
-    <div className="max-w-4xl py-10 mx-auto h-[100%] overflow-y-scroll">
+    <div className="max-w-4xl py-10 mx-auto h-[100%] overflow-y-auto">
+      <button
+        onClick={() => router.push('/projects')}
+        className="mb-6 px-4 py-2 bg-gray-800 text-gray-300 hover:bg-gray-700 text-white rounded-lg transition duration-200"
+      >
+        ← Retour à la liste des projets
+      </button>
+
       <h1 className="text-2xl font-bold mb-4">{project.title}</h1>
       <img
         src={project.image}
         alt={project.title}
         className="rounded-lg w-full object-cover mb-6"
-        />
+      />
       <p className="text-gray-400 mb-4">{project.description}</p>
       <h3 className="text-lg font-semibold mb-2">Technologies:</h3>
       <ul className="list-disc list-inside text-gray-300">
